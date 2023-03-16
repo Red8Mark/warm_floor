c9c031ebde1e22b577ab7514fb55a86d46472fdd
Red8Mark <work8red@gmail.com>
2023-03-17 01:16:30 +0900

    update

---
+++ js/script.js
@@ -0,0 +1,20 @@
+new Swiper('.hero__slider', {
+  slidesPerView: 2,
+  spaceBetween: 10,
+  loop: true,
+  navigation: {
+    prevEl: '.hero__slider-btn_prev',
+    nextEl: '.hero__slider-btn_next',
+  },
+  autoplay: {
+    delay: 3000,
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    560: {
+      spaceBetween: 8,
+    }
+  }
+})
