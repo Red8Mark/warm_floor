74cbf5ffc158406de2d82ea115b26cd271b97988
Red8Mark <work8red@gmail.com>
2023-03-17 13:45:24 +0900

    update

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// swiper slider
+
 new Swiper('.hero__slider', {
   slidesPerView: 2,
   spaceBetween: 10,
@@ -17,4 +19,31 @@ new Swiper('.hero__slider', {
       spaceBetween: 8,
     }
   }
-})
+});
+
+// calculator
+
+const calcForm = document.querySelector('.js-calc-form');
+const totalSquare = document.querySelector('.js-square');
+const totalPrice = document.querySelector('.js-total-price');
+const calcResult = document.querySelector('.js-result');
+
+const tariff = {
+  economy: 550,
+  comfort: 1400,
+  premium: 2700,
+}
+
+calcForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  if (calcForm.width.value > 0 && calcForm.length.value > 0) {
+    const square = calcForm.width.value * calcForm.length.value;
+    const price = square * tariff[calcForm.tariff.value];
+
+    calcResult.style.display = 'block';
+
+    totalSquare.textContent = `${square} кв м`;
+    totalPrice.textContent = `${price} руб`;
+  }
+});
